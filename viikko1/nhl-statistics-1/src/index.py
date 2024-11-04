d9a04e4d26e8c291752d446b09f6462abccd4a9b
kety <kety.kuusaru@helsinki.fi>
2024-11-04 19:48:06 +0200

    NHL stats injection added

--- viikko1/nhl-statistics-1/src/index.py
+++ viikko1/nhl-statistics-1/src/index.py
@@ -2,14 +2,9 @@ from statistics_service import StatisticsService
 from player_reader import PlayerReader
 
 def main():
-
-
-    stats = StatisticsService()
-    #uusi
-    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
-    reader = PlayerReader(url)
-    stats = StatisticsService(reader)
-    #uusi
+    stats = StatisticsService(
+        PlayerReader("https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt")
+    )
 
     philadelphia_flyers_players = stats.team("PHI")
     top_scorers = stats.top(10)
